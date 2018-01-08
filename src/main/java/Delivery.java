146e35a145b460bf7f3ce6b19bb88715961e3255
Lilleker, Robert <robert.lilleker@metoffice.gov.uk>
2018-01-08 15:02:43 +0000

    first lot of code

---
+++ src/main/java/Delivery.java
@@ -0,0 +1,33 @@
+/**
+ * Created by robert.lilleker on 08/01/2018.
+ */
+public class Delivery {
+    public int capacity;
+    public boolean XL = false;
+    public boolean L = false;
+    public boolean M = false;
+    public boolean S = false;
+
+    public Delivery (int cap){
+        this.capacity = cap;
+    }
+
+    public int getCost (){
+        int x=0;
+        if (XL) {
+            x=x+8;
+        }
+        if (L) {
+            x=x+4;
+        }
+        if (M) {
+            x=x+2;
+        }
+        if (S) {
+            x=x+1;
+        }
+
+        return x;
+    }
+
+}
---
+++ src/main/java/Machine.java
@@ -0,0 +1,38 @@
+/**
+ * Created by robert.lilleker on 08/01/2018.
+ */
+public class Machine {
+
+    public Machine(){
+
+    }
+
+    public int getCost(int cap){
+        Delivery d = new Delivery(cap);
+        this.calculate(d);
+        return d.getCost();
+    }
+
+    public void calculate(Delivery d) {
+        if (d.capacity > 9 ){
+            d.XL = true;
+            d.capacity = d.capacity - 5;
+        }
+        else if (d.capacity > 5){
+            d.L = true;
+            d.capacity = d.capacity - 4;
+        }
+        else if (d.capacity > 2) {
+            d.M = true;
+            d.capacity = d.capacity - 3;
+        }
+        else if (d.capacity > 0) {
+            d.S = true;
+            d.capacity = d.capacity - 2;
+        }
+
+        if (d.capacity > 0){
+            calculate(d);
+        }
+    }
+}
